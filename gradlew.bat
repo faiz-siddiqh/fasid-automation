c9014835da4f1801e9f7564ebf1294402dc7df0c
Faiz Ahmed Siddiqh K <67654574+faiz-siddiqh@users.noreply.github.com>
2022-10-17 22:43:57 +0530

    Update gradlew.bat
    Adding fix to print java path in case of error

--- gradlew.bat
+++ gradlew.bat
@@ -58,6 +58,7 @@ if exist "%JAVA_EXE%" goto execute
 
 echo.
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo %JAVA_EXE%
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
